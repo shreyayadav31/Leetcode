4af180a868103ac3f8c9fb684400b0008ce31477
shreyayadav31 <160255379+shreyayadav31@users.noreply.github.com>
2024-11-19 00:23:59 +0530

    Time: 3 ms (90.66%), Space: 12.3 MB (75.27%) - LeetHub

---
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string removeDuplicates(string s, int k) {
+        vector<pair<char,int>> st;
+        for(auto it:s){   //iterate through each character 
+            if(st.size()==0 || st.back().first!=it)    //
+            {
+                st.push_back({it,1});
+            }
+            else{
+                st.back().second++;      //element is repeated
+            }
+            if(st.back().second==k){
+                st.pop_back();
+            }
+        }
+        string res;
+        for(auto x:st){
+            res.append(x.second,x.first);  //insert st.first (char) st.second times (int)
+        }
+        return res;
+        
+    }
+};
